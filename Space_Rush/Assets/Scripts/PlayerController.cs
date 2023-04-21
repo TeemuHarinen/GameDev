4025dde32d0ab020e6fac5312a85b51a21120da0
Teemu Harinen <teemuharinen1@gmail.com>
2023-04-21 03:49:00 +0300

    Extended timeline and added rotation animation to movement

--- Space_Rush/Assets/Scripts/PlayerController.cs
+++ Space_Rush/Assets/Scripts/PlayerController.cs
@@ -5,13 +5,38 @@ public class PlayerController : MonoBehaviour
     [SerializeField] float xSpeedMultiplier = 50f;
     [SerializeField] float ySpeedMultiplier = 50f;
     [SerializeField] float xRange = 5f;
-    [SerializeField] float yRange = 5f;
+    [SerializeField] float yRange = 18f;
 
+    [SerializeField] float positionPitchFactor = -2f;
+    [SerializeField] float controlPitchFactor = -15f;
+    [SerializeField] float positionYawFactor = 2f;
+    [SerializeField] float controlRollFactor = -20f;
+
+    float yThrow, xThrow;
     
+
     void Update()
     {
-        float xThrow = Input.GetAxis("Horizontal");
-        float yThrow = Input.GetAxis("Vertical");
+        ProcessMovement();
+        ProcessRotation();
+    }
+
+    void ProcessRotation()
+    {
+        float pitchDueToPosition = transform.localPosition.y * positionPitchFactor;
+        float pitchDueToControl = yThrow * controlPitchFactor;
+
+        float pitch = pitchDueToPosition + pitchDueToControl;
+        float yaw = transform.localPosition.x * positionYawFactor;
+        float roll = xThrow * controlRollFactor;
+
+        transform.localRotation = Quaternion.Euler(pitch, yaw, roll);
+    }
+
+    void ProcessMovement()
+    {
+        xThrow = Input.GetAxis("Horizontal");
+        yThrow = Input.GetAxis("Vertical");
 
         float xOffset = xThrow * Time.deltaTime * xSpeedMultiplier;
         float rawXPos = transform.localPosition.x + xOffset;
@@ -19,9 +44,8 @@ public class PlayerController : MonoBehaviour
 
         float yOffset = yThrow * Time.deltaTime * ySpeedMultiplier;
         float rawYPos = transform.localPosition.y + yOffset;
-        float clampedYPos = Mathf.Clamp(rawYPos, -yRange + 3, yRange);
+        float clampedYPos = Mathf.Clamp(rawYPos, -yRange + 12, yRange);
 
         transform.localPosition = new Vector3(clampedXPos, clampedYPos, transform.localPosition.z);
-        
     }
 }
